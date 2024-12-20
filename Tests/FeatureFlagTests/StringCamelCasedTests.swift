58fa256ae0ff722e11840130ca44654877d390d0
KeitaroKawahara <keitaro.inc@gmail.com>
2024-12-20 20:52:50 +0900

    :sparkles: Initial commit

--- Tests/FeatureFlagTests/StringCamelCasedTests.swift
+++ Tests/FeatureFlagTests/StringCamelCasedTests.swift
@@ -2,43 +2,43 @@ import XCTest
 
 final class StringCamelCasedTests: XCTestCase {
 
-    func test_通常の文字列をキャメルケースに変換() {
+    func testCamelCased_withNormalString() {
         let input = "hello world"
         let expectedOutput = "helloWorld"
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_複数のスペースを含む文字列をキャメルケースに変換() {
+    func testCamelCased_withMultipleSpaces() {
         let input = "hello    world"
         let expectedOutput = "helloWorld"
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_特殊文字を含む文字列をキャメルケースに変換() {
+    func testCamelCased_withSpecialCharacters() {
         let input = "hello-world_test"
         let expectedOutput = "helloWorldTest"
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_大文字を含む文字列をキャメルケースに変換() {
+    func testCamelCased_withUppercaseCharacters() {
         let input = "HELLO WORLD"
         let expectedOutput = "helloWorld"
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_数字を含む文字列をキャメルケースに変換() {
+    func testCamelCased_withNumbers() {
         let input = "hello 123 world"
         let expectedOutput = "hello123World"
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_空文字列をキャメルケースに変換() {
+    func testCamelCased_withEmptyString() {
         let input = ""
         let expectedOutput = ""
         XCTAssertEqual(input.camelCased, expectedOutput)
     }
 
-    func test_単語のみの文字列をキャメルケースに変換() {
+    func testCamelCased_withSingleWord() {
         let input = "hello"
         let expectedOutput = "hello"
         XCTAssertEqual(input.camelCased, expectedOutput)
